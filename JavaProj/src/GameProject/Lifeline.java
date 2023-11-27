6b3e4c1c6fc5e1d97d63c65ef449347d6a0a00be
DEELIN WONG <dc.wang547@gmail.com>
2023-11-27 07:50:08 -0500

    Lifeline setup

--- JavaProj/src/GameProject/Lifeline.java
+++ JavaProj/src/GameProject/Lifeline.java
@@ -1,7 +1,35 @@
 package GameProject;
+import java.util.Scanner;
 
 public class Lifeline {
+	
+	private Lifeline50_50 lifeline5050 = new Lifeline50_50();
+	private LifelineAskAudience lifelineAudience = new LifelineAskAudience();
+	private LifelinePhoneFriend lifelineFriend = new LifelinePhoneFriend();
+	
     public void runLifeline() {
-        System.out.println("Generic lifeline used.");
+    	Scanner s = new Scanner(System.in);
+		System.out.println("Select the type of Lifeline (you can only use each once!):");
+		System.out.println("A or a - 50/50--> This lifeline allows the game to eliminate two incorrect answers");
+		System.out.println("B or b - Ask the Audience--> This lifeline allows the player to “ask the audience”for the correct answer. Be creative with regards to how you implement this.");
+		System.out.println("C or c - Phone a friend --> This lifeline allows the player to “phone to afriend” for the correct answer. Be creative with regards to how you implement this");		
+		System.out.print("\nYour selection:");
+		String option = s.nextLine();
+		
+		if (option == "a" || option == "A") {
+			//lifeline 50/50
+			lifeline5050.runLifeline();
+			
+			
+		}else if (option == "b" || option == "B") {
+			//ask audience
+			lifelineAudience.runLifeline();
+			
+		}else if (option == "c" || option == "C") {
+			//phone a friend
+			lifelineFriend.runLifeline();
+		}else {
+			System.out.println("Invaid choice, Please enter a valid choice from letter A-C: ");
+		}
     }
 }
--- JavaProj/src/GameProject/Lifeline50_50.java
+++ JavaProj/src/GameProject/Lifeline50_50.java
@@ -1,8 +1,13 @@
 package GameProject;
+import java.util.Scanner;
 
 public class Lifeline50_50 extends Lifeline {
+	
+//	private Question question = new Question();
     @Override
     public void runLifeline() {
-        System.out.println("Eliminate two incorrect answers.");
+    	
+    	//
+    	
     }
 }
