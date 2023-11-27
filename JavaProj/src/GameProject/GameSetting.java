9df98a7419a06cbcb466fc07224014290a6526f1
DEELIN WONG <dc.wang547@gmail.com>
2023-11-27 08:33:06 -0500

    merge conflict resolve

--- JavaProj/src/GameProject/GameSetting.java
+++ JavaProj/src/GameProject/GameSetting.java
@@ -7,20 +7,12 @@ public class GameSetting {
     private int difficulty;
     private static int question = 1;
     private static int round = 1;
-<<<<<<< HEAD
+    
     private int[] questionValuesEasy = {100, 500, 1000, 8000, 16000, 32000, 125000, 500000, 1000000}; //9 values for Easy
     private int[] questionValuesHard = {100, 200, 300, 500, 1000, 2000, 4000, 8000, 16000, 32000, 64000, 125000, 250000, 500000, 1000000}; //15 values for hard
-    private int[] usedLifeLines; //store lifelines that has been used, if empty: player never used lifeline
+    private String[] usedLifeLines; //store lifelines that has been used, if empty: player never used lifeline
     private double prizeMoney = 0;
     
-=======
-//    private int[] questionValuesEasy;
-//    private int[] questionValuesHard;
-    private boolean usedLifeLine = false;  
-  private static boolean isCorrect = false;
-//  feel like we might not need to track this
-    private int[] usedLifeLines;
->>>>>>> 30abefc1b1cbb4fcbfb201c4ccccf9c2bfeec569
     
     public String getCurrentScreen() {
         return screen;
@@ -66,7 +58,6 @@ public class GameSetting {
     	this.round= num;
     }
     
-<<<<<<< HEAD
     public double returnPrize(){
     	return prizeMoney;
     }
@@ -75,9 +66,6 @@ public class GameSetting {
     	this.prizeMoney+= prize;
     }
     
-=======
->>>>>>> 30abefc1b1cbb4fcbfb201c4ccccf9c2bfeec569
-
     public void retrieveStoredData() {
         // Implement logic to retrieve stored data
     }
