70fb59d4e8c9ef7f5dbe81a23cedc8eabbbb72ab
DEELIN WONG <dc.wang547@gmail.com>
2023-12-01 22:37:43 -0500

    Lifeline 5050 draft

--- JavaProj/src/GameProject/Lifeline50_50.java
+++ JavaProj/src/GameProject/Lifeline50_50.java
@@ -1,14 +1,42 @@
 package GameProject;
-import java.util.Scanner;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Lifeline50_50 extends Lifeline {
 	
-	Lifeline50_50(){}
-	
-//	private Question question = new Question();
+
     @Override
     public void runLifeline() {
-        // Implement logic to eliminate two incorrect answers
-        System.out.println("You've used the 50/50 lifeline. Two incorrect answers have been eliminated.");
+    
+    	
+    	Question currentQuestion = GameSetting.getCurrentQuestion();
+        String[] choices = currentQuestion.getChoices();
+        int correctIndex = currentQuestion.getCorrectAnsIndex();
+
+        // Create a list to track the indices of incorrect choices
+        ArrayList<Integer> incorrectIndices = new ArrayList<>();
+        for (int i = 0; i < choices.length; i++) {
+            if (i != correctIndex) {
+                incorrectIndices.add(i);
+            }
+        }
+
+        // Randomly select and eliminate two incorrect choices
+        Random random = new Random();
+        for (int i = 0; i < 2; i++) {
+            int randomIndex = random.nextInt(incorrectIndices.size());
+            int indexToRemove = incorrectIndices.get(randomIndex);
+            choices[indexToRemove] = ""; // Eliminate the choice by emptying it
+            incorrectIndices.remove(randomIndex);
+        }
+
+        // Display the updated question with the eliminated choices
+        System.out.println("Two incorrect choices have been eliminated:");
+        for (int i = 0; i < choices.length; i++) {
+            if (!choices[i].isEmpty()) {
+                System.out.println(Question.letterChoices[i] + ".) " + choices[i]);
+            }
+        }
     }
 }
